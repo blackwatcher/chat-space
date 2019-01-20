f08b5b01f8efa3018333d7c6bd7cd0cb11d04870
内田啓太 <keita.uchida.r@gmail.com>
2019-01-20 10:16:24 +0900

    update-variable-name
    変数名をわかりやすく変更。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,13 @@
 $(function(){
   function buildHtml(msg){
-    if (msg.content && msg.image.url) var t = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><p class="lower-message__content">' + msg.content + '</p><img src="' + msg.image.url + '" class="lower-message__image" ></div></div>';
-    else if (msg.content) var t = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><p class="lower-message__content">' + msg.content + "</p></div></div>";
-    else if (msg.image.url) var t = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><img src="' + msg.image.url + '" class="lower-message__image" ></div></div>';
-    return t
+    if (msg.content && msg.image.url) var msgType = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><p class="lower-message__content">' + msg.content + '</p><img src="' + msg.image.url + '" class="lower-message__image" ></div></div>';
+    else if (msg.content) var msgType = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><p class="lower-message__content">' + msg.content + "</p></div></div>";
+    else if (msg.image.url) var msgType = '<div class="message"><div class="upper-message"><div class="upper-message__user-name">' + msg.user_name + '</div><div class="upper-message__date">' + msg.created_at + '</div></div><div class="lower-message"><img src="' + msg.image.url + '" class="lower-message__image" ></div></div>';
+    return msgType
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var n = $(this);
+    var msgArea = $(this);
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -20,7 +20,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHtml(data);
-      $messages = $('.messages').append(html), n[0].reset(), $messages.animate({ scrollTop: $messages[0].scrollHeight }, 1500)
+      $messages = $('.messages').append(html), msgArea[0].reset(), $messages.animate({ scrollTop: $messages[0].scrollHeight }, 1500)
     })
     .fail(function(){
       alert('error');
