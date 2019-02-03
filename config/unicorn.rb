d54e6c88c35da02848ef34d738b7160f9e6d32fa
内田啓太 <keita.uchida.r@gmail.com>
2019-02-03 12:15:57 +0900

    Update-unicorn.rb
    Update-unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 app_path = File.expand_path('../../../', __FILE__)
 working_directory "/var/www/chat-space/"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "/var/www/chat-space/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
